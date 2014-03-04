d66988e545c14495f56420b6f1c3dacf233ead12
rxi <rxi@users.noreply.github.com>
2014-03-04 16:40:17 +0000

    Changed round() from global to local

--- lib/underscore.lua
+++ lib/underscore.lua
@@ -829,7 +829,7 @@ function _.capitalize(str)
   return str:gsub("^%l", string.upper)
 end
 
-function round(num, idp)
+local function round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
